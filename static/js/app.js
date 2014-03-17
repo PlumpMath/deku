1231dbfcc95aef7f8cc1c512716ed65bd09575c1
Boris Boiko <b.sboiko@umbc.edu>
2014-03-17 19:35:08 -0400

    Cleaned up the design of the collapsing menus. Only one is open at a time and the add card option is open by default. Added user entry section for cards and search.

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,21 @@ function collapse() {
 	//uses ul tags to find contain what is hidden
 	$('#collapsed-list').find('li:has(ul)')
    .click(function (event) {
-     if (this == event.target) {
-        $(this).toggleClass('expanded');
-        $(this).children('ul').toggle('medium');
-       }
+		 //This prevents an open tab from being closed and opened
+		 if (!$(this).hasClass('expanded')) {
+    	 if (this == event.target) {
+				 closeAll();
+     	   $(this).toggleClass('expanded');
+     	   $(this).children('ul').toggle('medium');
+     	 }
+		 }
      return false;
     })
-		//everything starts off collapsed
     .addClass('collapsed')
     .children('ul').hide();
+};
+
+function closeAll() {
+	$('.collapsed').removeClass('expanded');
+	$('.collapsed').children().hide('medium');
 };
