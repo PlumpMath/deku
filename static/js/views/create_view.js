050a0ceae4ddb6907e786187b39550c4c2fae728
kuo1 <kuo1@umbc.edu>
2014-03-26 16:59:05 -0400

    attempted to modify create_view.js to work with the register function.  can't figure how to access .fadeout

--- static/js/views/create_view.js
+++ static/js/views/create_view.js
@@ -22,7 +22,40 @@ app.CreateView = Backbone.View.extend({
 
   getInfo: function(event) {
     event.preventDefault();
-		this.$el.fadeOut(350, function() {new app.InfoView();});
+		//This will contain some event that sends the data to the server
+		//for authentication
+		//this is the app.py route info
+		var url = '../../../register';
+
+		//this is the data that is sent
+		var registerValues = {
+			email: $("#email").val(),
+			password: $("#password").val(),
+			university: $("#univ").val(),
+			name: $("#name").val()
+		};
+		
+		//this is the ajax post request
+		$.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: registerValues,
+			success: function(data) {
+				if(typeof data.errors == "undefined"){
+					alert("i cant figure out how to do el fadeout thing. get it get it? el=the?");
+					$("el").fadeOut(350, function() {new app.InfoView();});
+				}else{
+					alert(data.errors);
+				}
+			},
+			error: function() {
+				alert('fail');
+				$('#lemail').val('');
+				$('#lpassword').val('');
+				$('#lemail').focus();
+			}
+		});
   }
 
 });
