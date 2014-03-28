18446563fc9d0d430ca41387da1f0c22f32d815c
Boris Boiko <b.sboiko@umbc.edu>
2014-03-28 21:11:45 +0000

    Added form validation to most of the existing forms. Reset and Search are not done, but they are implemented a bit different anyways. For now, if the input is valid at Create Account, the code will step through to the next page regardless of server response; this is temporary.

--- static/js/views/create_card_view.js
+++ static/js/views/create_card_view.js
@@ -19,19 +19,49 @@ app.CreateCardView = Backbone.View.extend({
 		this.$el.html(this.template);
 	},
 
+  formError: function(values) {
+    error = false;
+    category_list = $('#categories').children().map(function() { return this.value;}).get();
+
+    if (values.content === '') {
+      error = true;
+      $('#content').val('')
+      .attr('placeholder', 'Share with your university')
+      .focus();
+    }
+
+    if (values.tags === '') {
+      error = true;
+      $('#tags').val('')
+      .attr('placeholder', 'Enter tags')
+      .focus();
+    }
+
+    if (values.category === '' || $.inArray(values.category, category_list) === -1) {
+      error = true;
+      $('#category').val('')
+      .attr('placeholder', 'Select a category')
+      .focus();
+    }
+
+    return error;
+  },
+
 	addCard: function(event) {
 		event.preventDefault();
 		var formData = {
 			category: $('#category').val(),
-			tags: $('#tags').val(),
-			content: $('#content').val()
+			tags: $('#tags').val().trim(),
+			content: $('#content').val().trim()
 		};
 
-		var card = new app.Card(formData);
+    if (!this.formError(formData)) {
+  		var card = new app.Card(formData);
 
-		this.vent.trigger("newCard", card);
-		$('#category').val('');
-		$('#tags').val('');
-		$('#content').val('');
+	  	this.vent.trigger("newCard", card);
+  		$('#category').val('');
+  		$('#tags').val('');
+  		$('#content').val('');
+    }
 	}
 });
--- static/js/views/info_view.js
+++ static/js/views/info_view.js
@@ -17,8 +17,59 @@ app.InfoView = Backbone.View.extend({
     this.$el.html(this.template).fadeIn(350);
   },
 
+	formErrors: function(values) {
+		error = false;
+    major_list = $('#major').children().map(function() { return this.value;}).get();
+    year_list = $('#year').children().map(function() { return this.value;}).get();
+
+		if (values.bio === '') {
+			error = true;
+			$('#bio').val('')
+			.attr('placeholder', 'Please tell us something about yourself')
+      .focus();
+		}
+		
+    if (values.classes === '') {
+			error = true;
+			$('#classes').val('')
+			.attr('placeholder', 'Please enter your classes this semester')
+      .focus();
+		}
+
+		//Right now this only checks for an empty string.
+		//Eventually it should compare against the possible list of options
+		//and reject it if it does not match something in it.
+		if (values.major === '' || $.inArray(values.major, major_list) === -1) {
+			error = true;
+			$('#major-list').val('')
+			.attr('placeholder', 'Please select your major from the given list')
+      .focus();
+		}
+
+		//eventually, we might want to consider adding an upper bound
+		//to the grad year
+		if (values.year === '' || $.inArray(values.year, year_list) === -1){
+			error = true;
+			$('#grad-year').val('')
+			.attr('placeholder', 'Please enter a valid graduation year')
+      .focus();
+		}
+
+		return error;
+	},
+
   getLogin: function(event) {
     event.preventDefault();
-		this.$el.fadeOut(350, function() {new app.LoginView();});
+		
+		values = {
+			year: $('#grad-year').val(),
+			major: $('#major-list').val(),
+			classes: $('#classes').val().trim(),
+			bio: $('#bio').val().trim()
+		};
+
+		if (!this.formErrors(values)) {
+			this.$el.fadeOut(350, function() {new app.LoginView();});
+		}
   }
 });
