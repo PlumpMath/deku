1a94c11048bc5073cfa15c1ce38e5b5d47e7c81b
kuo1 <kuo1@umbc.edu>
2014-04-02 07:42:50 -0400

    added profile table.  added authentication via tokens or user:pass.  sample usage in /deku/api/users/<int:user_id>PUT.

--- tests.py
+++ tests.py
@@ -42,55 +42,101 @@ class APITestCase(unittest.TestCase):
 
     # Test /deku/api/users GET
     def test_get_all_users(self):
+        print 'GETALLUSERS'
         response = self.app.get('/deku/api/users')
         self.assertIsNotNone(response.get_data())
 
     # Test /deku/api/users POST
     def test_post_new_user(self):
+        print 'POSTNEWUSER'
+        password = bcrypt.generate_password_hash('somepassword')
         response = self.app.post('/deku/api/users', data = dict(
-            firstName = "Carrie",
             lastName = "Hildebrand",
+            firstName = "Carrie",
             email = "carrie.hildebrand@gmail.com",
-            password = bcrypt.generate_password_hash('password2')))
+            password = password,
+            univ='un'))
         self.assertEquals(response.status_code, 201)
 
     def test_post_new_user_bad_request(self):
+        print 'POSTNEWUSERBADREQUEST'
         response = self.app.post('/deku/api/users', data = dict(
             firstName = "Peter"))
         self.assertEquals(response.status_code, 400)
 
-    # Test /deku/api/users/<int:user_id> GET    
+    # Test /deku/api/users/<int:user_id> GET
     def test_get_user(self):
+        print 'GETUSER'
         response = self.app.get('/deku/api/users/1')
         self.assertIn("John", response.get_data())
 
     def test_get_user_fail(self):
-        response = self.app.get('/deku/api/users/5')
+        print 'GETUSERFAIL'
+        response = self.app.get('/deku/api/users/578333')
         self.assertEquals(response.status_code, 404)
-
+        
     # Test /deku/api/users/<int:user_id> PUT
-    def test_mod_user(self):
-        response = self.app.put('/deku/api/users/1', data=dict( firstName="Lucas"))
+    def test_mod_user_success_with_email_pwd(self):
+        print 'MODUSER'
+        response = self.app.put('/deku/api/users/1', data=dict( firstName="Lucas", email="johndoe@email.com", password="password"))
         self.assertIn("Lucas", response.get_data())
 
+    # Test /deku/api/users/<int:user_id> PUT
+    def test_mod_user_success_with_id_token(self):
+        #get token first
+        response = self.app.post('/deku/api/users/login', data = dict(
+            email="johndoe@email.com",
+            password="password"))
+        data = json.loads(response.data)
+        id = data['user']['id']
+        token = data['token']
+        #use token
+        response = self.app.put('/deku/api/users/1', data=dict( firstName="Lucas", token=token))
+        self.assertIn("Lucas", response.get_data())
+
+    # Test /deku/api/users/<int:user_id> PUT
+    def test_mod_user_fail_authentication(self):
+        print 'MODUSER'
+        response = self.app.put('/deku/api/users/1', data=dict( firstName="Lucas"))
+        self.assertEquals(response.status_code, 401)
+
     def test_mod_user_fail(self):
-        response = self.app.put('/deku/api/users/5', data=dict( lastName="Hood"))
+        print 'MODUSERFAIL'
+        response = self.app.put('/deku/api/users/7', data=dict( lastName="Hood"))
         self.assertEquals(response.status_code, 404)
 
     # Test /deku/api/users/<int:user_id> DELETE
     def test_delete_user(self):
+        print 'DELETE_USER'
         response = self.app.delete('/deku/api/users/1')
         self.assertEquals(response.status_code, 200)
 
     def test_delete_user_fail(self):
+        print 'DELETEUSERFAIL'
         response = self.app.delete('/deku/api/users/8')
         self.assertEquals(response.status_code, 204)
 
     def test_user_login(self):
+        print 'USERLOGIN USING EMAIL PASSWORD'
         response = self.app.post('/deku/api/users/login', data = dict(
             email="johndoe@email.com",
             password="password"))
         self.assertEquals(response.status_code, 200)
+        data = json.loads(response.data)
+        id = data['user']['id']
+        token = data['token']
+        
+        print 'USERLOGIN USING id and token'
+        response = self.app.post('/deku/api/users/login', data = dict(
+            id=id,
+            token=token))
+        self.assertEquals(response.status_code, 200)
+        
+    def test_user_login_invalid_token(self):
+        response = self.app.post('/deku/api/users/login', data = dict(
+            id=1,
+            token="123456786asdfqa442455"))
+        self.assertEquals(response.status_code, 401)
 
     def test_user_login_bad_password(self):
         response = self.app.post('/deku/api/users/login', data = dict(
@@ -102,7 +148,7 @@ class APITestCase(unittest.TestCase):
         response = self.app.post('/deku/api/users/login', data = dict(
             email="boristhesovietlovehammer@motherrussia.ru",
             password="putinismycomrade"))
-        self.assertEquals(response.status_code, 404)
+        self.assertEquals(response.status_code, 401)
 
     # Test /deku/api/cards GET
     def test_get_all_cards(self):
