137f06bb96aca6473e9d6ab3fe3327d8d2e038cc
Boris Boiko <b.sboiko@umbc.edu>
2014-05-02 22:52:44 +0000

    When viewing a notification, you can click the name of the given user to go to their profile.

--- js/views/notification_view.js
+++ js/views/notification_view.js
@@ -6,6 +6,11 @@ app.NotificationView = Backbone.View.extend({
 
   template: "#notification-view",
 
+  events: {
+    "click #user-profile-notification": "goToProfile",
+    "click #card-notification": "goToCard"
+  },
+
 	initialize: function() {
 		this.render();
 	},
@@ -14,6 +19,16 @@ app.NotificationView = Backbone.View.extend({
     var template = app.TemplateCache.get(this.template);
     var html = template(this.model.toJSON())
 		this.$el.append(html);
-	}
+	},
+
+  /* This will route the user to the profile page of the author of this card.
+   * It will use the data in the card model and parse the name and get the id
+   * The router will handle the rest.
+   */
+  goToProfile: function(event) {
+    event.preventDefault();
+    // navigate to the route for the user's profile
+    app.router.navigate('profile/' + $(event.target).attr('name'), {trigger: true});
+  }
 
 });
